0a73a11828901190f2adaa023387f3f556b25f31
luphihart <lupiyohartadi@gmail.com>
2026-07-10 16:51:27 +0700

    Override public path in bootstrap/app.php dynamically for cPanel subfolder deployment, fixing Vite manifest not found exception

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -18,3 +18,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// Dynamic public path override for cPanel deployment
+$cpanelPublicPath = '/home/sina4714/public_html/pklku.sinaumedia.my.id';
+if (is_dir($cpanelPublicPath)) {
+    $app->usePublicPath($cpanelPublicPath);
+}
+
+return $app;
