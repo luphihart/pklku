4ba858feb47d12e201b7267bdfc9e8ea96d7727f
luphihart <lupiyohartadi@gmail.com>
2026-07-10 15:36:12 +0700

    Fix nested form bugs

--- app/Modules/MasterData/Views/guru/index.blade.php
+++ app/Modules/MasterData/Views/guru/index.blade.php
@@ -67,14 +67,11 @@
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
                                             </svg>
                                         </button>
-                                        <form action="{{ route('guru.reset_password', $guru->id) }}" method="POST" onsubmit="return confirm('Apakah Anda yakin ingin mereset password guru ini menjadi default (guru123)?');" style="display: inline-block;">
-                                            @csrf
-                                            <button type="submit" class="btn btn-sm btn-outline-info p-1" title="Reset Password (guru123)">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7a2 2 0 012 2m-2-2a2 2 0 11-2-2m2 2a2 2 0 002 2m0 0a2 2 0 002-2v3a2 2 0 01-2 2h-1a2 2 0 01-2-2v-5a2 2 0 00-2-2H9m0 0l-2 2m2-2l-2-2M7 9v1H6v1H5v1H4v1H3v1h1"/>
-                                                </svg>
-                                            </button>
-                                        </form>
+                                        <button type="button" class="btn btn-sm btn-outline-info p-1" title="Reset Password (guru123)" onclick="if(confirm('Apakah Anda yakin ingin mereset password guru ini menjadi default (guru123)?')) { document.getElementById('resetForm_{{ $guru->id }}').submit(); }">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7a2 2 0 012 2m-2-2a2 2 0 11-2-2m2 2a2 2 0 002 2m0 0a2 2 0 002-2v3a2 2 0 01-2 2h-1a2 2 0 01-2-2v-5a2 2 0 00-2-2H9m0 0l-2 2m2-2l-2-2M7 9v1H6v1H5v1H4v1H3v1h1"/>
+                                            </svg>
+                                        </button>
                                         <button type="button" class="btn btn-sm btn-outline-danger p-1" title="Hapus Guru" onclick="if(confirm('Apakah Anda yakin ingin menghapus guru ini? Akun login terkait juga akan ikut dihapus.')) { document.getElementById('deleteForm_{{ $guru->id }}').submit(); }">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
@@ -98,6 +95,13 @@
             <form action="{{ route('guru.destroy', $guru->id) }}" method="POST" id="deleteForm_{{ $guru->id }}" style="display: none;">
                 @csrf
                 @method('DELETE')
+            </form>
+        @endforeach
+
+        <!-- Hidden Reset Password Forms -->
+        @foreach($gurus as $guru)
+            <form action="{{ route('guru.reset_password', $guru->id) }}" method="POST" id="resetForm_{{ $guru->id }}" style="display: none;">
+                @csrf
             </form>
         @endforeach
 
